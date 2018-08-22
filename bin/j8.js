2dc29fcaf06e2a352b178c3cf7aa963133f10e3d
xxj95719 <87416959@qq.com>
2018-08-22 14:43:30 +0800

    🐢

--- bin/j8.js
+++ bin/j8.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-const path = require('path')
 const fs = require('fs')
 
 const program = require('commander') //可以解析用户输入的命令
 const download = require('download-git-repo') //拉取github上的文件
 const chalk = require('chalk') //改变输出文字的颜色
+const inquirer = require('inquirer')
 const ora = require('ora') //小图标（loading、succeed、warn等）
 
 program
-  .version('0.0.1')
-  .option('-i, init [name]', '初始化j8项目')
+  .version(require('../package').version)
+  .option('-i, init', '初始化j8项目')
   .parse(process.argv)
 
 program.parse(process.argv)
@@ -25,7 +25,7 @@ const versionQuestion = {
   type: 'input',
   message: `初始版本: `,
   name: 'version',
-  default: '0.0.1'
+  default: require('../package').version
 }
 
 const portQuestion = {
@@ -47,7 +47,7 @@ if (program.init) {
   inquirer
     .prompt([nameQuestion, versionQuestion, portQuestion, templateQuestion])
     .then(function(answers) {
-      const spinner = ora('正在从github下载x-build').start()
+      const spinner = ora('正在从github下载...').start()
       download('xxj95719/multi-page-app', answers.name, function(err) {
         if (!err) {
           spinner.clear()
@@ -74,35 +74,6 @@ if (program.init) {
             chalk.green('-----------------------------------------------------')
           )
           console.info('')
-
-          if (answers.template === true) {
-            fs.unlinkSync(`${process.cwd()}/${answers.name}/src/index.html`)
-          } else {
-            fs.unlinkSync(`${process.cwd()}/${answers.name}/index.pug`)
-            fs.unlinkSync(`${process.cwd()}/${answers.name}/src/app.pug`)
-          }
-
-          fs.readFile(
-            `${process.cwd()}/${answers.name}/package.json`,
-            (err, data) => {
-              if (err) throw err
-              let _data = JSON.parse(data.toString())
-              _data.name = answers.name
-              _data.version = answers.version
-              _data.port = answers.port
-              _data.template = answers.template ? 'pug' : 'html'
-              _data.rem = answers.rem
-              let str = JSON.stringify(_data, null, 4)
-              fs.writeFile(
-                `${process.cwd()}/${answers.name}/package.json`,
-                str,
-                function(err) {
-                  if (err) throw err
-                  process.exit()
-                }
-              )
-            }
-          )
         } else {
           spinner.warn([
             '发生错误，请在https://github.com/xxj95719/，Issues留言'
