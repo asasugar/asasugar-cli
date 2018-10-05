00ea55815f6f1661881bdd94434b5c5e3999ee79
xxj95719 <87416959@qq.com>
2018-10-05 19:30:40 +0800

    加入vue、react模板

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # j8-cli
 
-> 自用生成多页应用模板 cli 工具（目前支持生成 html 跟 pug 两种方式，后续将继续添加vue模板、react模板...）
+> 自用生成多页应用模板 cli 工具（目前支持生成 html 跟 pug 两种方式），单页应用支持 vue 模板、react 模板
 
 ## Start
 
 ```bash
 # 全局安装cli
 npm i j8-cli -g
+
+# 查看版本
+j8 -V
 
 # 创建项目
 j8 init
--- bin/j8.js
+++ bin/j8.js
@@ -8,126 +8,120 @@ const inquirer = require('inquirer')
 const ora = require('ora') //小图标（loading、succeed、warn等）
 const package = require('../package')
 program
-  .version(require('../package').version)
+  .version(package.version)
   .option('-i, init', '初始化j8项目')
   .parse(process.argv)
 
 program.parse(process.argv)
 
-const nameQuestion = {
-  type: 'input',
-  message: `项目名称: `,
-  name: 'name',
-  default: 'my-project'
-}
-const desQuestion = {
-  type: 'input',
-  message: `项目描述: `,
-  name: 'description',
-  default: package.description
-}
-
-const versionQuestion = {
-  type: 'input',
-  message: `初始版本: `,
-  name: 'version',
-  default: package.version
-}
-
-const portQuestion = {
-  type: 'input',
-  message: `server端口: `,
-  name: 'port',
-  default: '8080'
-}
-
-const templateQuestion = {
-  type: 'confirm',
-  message: `使用pug(jade)模版引擎? `,
-  name: 'template',
-  default: true
-}
+const question = [
+  {
+    type: 'list',
+    message: `请选择项目类型? `,
+    name: 'type',
+    choices: ['MPA(多页应用)', 'Vue', 'React']
+  },
+  {
+    type: 'input',
+    message: `项目名称: `,
+    name: 'name',
+    default: 'my-project'
+  },
+  {
+    type: 'input',
+    message: `项目描述: `,
+    name: 'description',
+    default: 'Description'
+  },
+  {
+    type: 'input',
+    message: `初始版本: `,
+    name: 'version',
+    default: '0.0.1'
+  },
+  {
+    type: 'input',
+    message: `server端口: `,
+    name: 'port',
+    default: '8080'
+  },
+  {
+    type: 'confirm',
+    message: `使用pug(jade)模版引擎? `,
+    name: 'template',
+    when: answers => answers.type === 'MPA(多页应用)',
+    default: true
+  }
+]
 
 if (program.init) {
   console.info('')
-  inquirer
-    .prompt([
-      nameQuestion,
-      desQuestion,
-      versionQuestion,
-      portQuestion,
-      templateQuestion
-    ])
-    .then(function(answers) {
-      const spinner = ora('正在从github下载...').start()
-      download(
-        answers.template
-          ? 'xxj95719/multi-page-app#pug'
-          : 'xxj95719/multi-page-app#html',
-        answers.name,
-        function(err) {
-          if (!err) {
-            spinner.clear()
-            console.info('')
-            console.info(
-              chalk.green(
-                '-----------------------------------------------------'
-              )
-            )
-            console.info('')
-            spinner.succeed(['项目创建成功,请继续进行以下操作:'])
-            console.info('')
-            console.info(chalk.cyan(` -  cd ${answers.name}`))
-            console.info(chalk.cyan(` -  npm install / yarn`))
-            console.info(chalk.cyan(` -  npm run dev`))
-            console.info('')
-            console.info(
-              chalk.gray(`devServer: http://localhost:${answers.port}`)
-            )
-            console.info('')
-            console.info(
-              chalk.gray(
-                '参考文档: https://github.com/xxj95719/multi-page-app/tree/' +
-                answers.template
-                  ? 'pug'
-                  : 'html'
-              )
-            )
-            console.info('')
-            console.info(
-              chalk.green(
-                '-----------------------------------------------------'
-              )
-            )
-            console.info('')
+  inquirer.prompt(question).then(function(answers) {
+    let downloadUrl
+    if (answers.type === 'MPA(多页应用)') {
+      // MPA
+      downloadUrl = answers.template
+        ? 'xxj95719/multi-page-app#pug'
+        : 'xxj95719/multi-page-app#html'
+    } else if (answers.type === 'Vue') {
+      // Vue
+      downloadUrl = 'xxj95719/vue-spa'
+    } else if (answers.type === 'React') {
+      // React
+      downloadUrl = 'xxj95719/react-spa'
+    }
+    const spinner = ora('正在从github下载...').start()
+    download(downloadUrl, answers.name, function(err) {
+      if (!err) {
+        spinner.clear()
+        console.info('')
+        console.info(
+          chalk.green('-----------------------------------------------------')
+        )
+        console.info('')
+        spinner.succeed(['项目创建成功,请继续进行以下操作:'])
+        console.info('')
+        console.info(chalk.cyan(` -  cd ${answers.name}`))
+        console.info(chalk.cyan(` -  npm install / yarn`))
+        console.info(chalk.cyan(` -  npm run dev / yarn dev`))
+        console.info('')
+        console.info(chalk.gray(`devServer: http://localhost:${answers.port}`))
+        console.info('')
+        console.info(
+          chalk.gray('参考文档: https://github.com/xxj95719/j8-cli/')
+        )
+        console.info('')
+        console.info(
+          chalk.green('-----------------------------------------------------')
+        )
+        console.info('')
 
-            fs.readFile(
+        fs.readFile(
+          `${process.cwd()}/${answers.name}/package.json`,
+          (err, data) => {
+            if (err) throw err
+            let _data = JSON.parse(data.toString())
+            _data.name = answers.name
+            _data.description = answers.description
+            _data.version = answers.version
+            _data.port = answers.port
+            let str = JSON.stringify(_data, null, 4)
+            fs.writeFile(
               `${process.cwd()}/${answers.name}/package.json`,
-              (err, data) => {
+              str,
+              function(err) {
                 if (err) throw err
-                let _data = JSON.parse(data.toString())
-                _data.name = answers.name
-                _data.description = answers.description
-                _data.version = answers.version
-                _data.port = answers.port
-                let str = JSON.stringify(_data, null, 4)
-                fs.writeFile(
-                  `${process.cwd()}/${answers.name}/package.json`,
-                  str,
-                  function(err) {
-                    if (err) throw err
-                    process.exit()
-                  }
-                )
+                process.exit()
               }
             )
-          } else {
-            spinner.warn([
-              '发生错误，请在https://github.com/xxj95719/j8-cli/issues，Issues留言'
-            ])
-            process.exit()
           }
-        }
-      )
+        )
+      } else {
+        spinner.warn([
+          '发生错误，请在https://github.com/xxj95719/j8-cli/issues，Issues留言'
+        ])
+        process.exit()
+      }
     })
+  })
 }
